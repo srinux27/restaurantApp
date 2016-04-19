aa3b99e60f48666e2ade48c56d96f496d06f64ad
srinux27 <srinux27@gmail.com>
2016-04-19 16:55:35 +0530

    Updated for REST API created with deployd tool.

--- js/appFactory.js
+++ js/appFactory.js
@@ -1,42 +1,37 @@
-(function() {
+(function () {
 
     var factModule = angular.module('FactoryModule', ['ngResource']);
-    
-    factModule.factory('ItemFactory', function() {
-        var menuItems = [
-            {name:'Paneer Butter Masala', price: 350, code: 'VG100'},
-            {name:'Kadai Paneer', price: 375, code: 'VG101'},
-            {name:'Vegetarian Butter Gravy', price: 175, code: 'VG102'},
-            {name:'Chicken Tikka Masala', price: 450, code: 'NG100'}, 
-            {name:'Chicken Biriyani', price: 350, code: 'NG101'},
-            {name:'Egg Masala', price: 250, code: 'EG100'},
-            {name:'Special Omelet', price: 199, code: 'EG101'}
-        ];
+
+    factModule.factory('ItemFactory', function ($resource) {
+        var menuItems;
         
+        var menuResource = $resource('http://localhost:2403/menuitems');
+
         return {
-            getMenuItems:function() {
-                return menuItems;    
+            getMenuItems: function () {
+                menuItems = menuResource.query();
+                return menuItems;
             }
         };
     });
-    
-    
-    factModule.factory('OrderFactory', function() {
+
+
+    factModule.factory('OrderFactory', function () {
         var orderedItems = [];
-        
+
         return {
-            getOrderedItems : function() {
+            getOrderedItems: function () {
                 return orderedItems;
-            },
-            addOrderedItems : function(item) {
+            }
+            , addOrderedItems: function (item) {
                 orderedItems.push(item);
-            },
-            deleteOrderedItems : function(itemIndex) {
+            }
+            , deleteOrderedItems: function (itemIndex) {
                 orderedItems.splice(itemIndex, 1);
-                
+
             }
         }
-        
+
     });
 
 }());
