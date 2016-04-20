5b4b9cf547bcbe38a6af5b721e8003f23f499f4b
srinux27 <srinux27@gmail.com>
2016-04-20 08:47:01 +0530

    Updating some changes

--- js/appService.js
+++ js/appService.js
@@ -6,12 +6,12 @@
             return ItemFactory.getMenuItems();
         };
         
-        this.placeOrder = function(code) {
+        this.placeOrder = function(menuItem) {
             var itemFound =  false;
             var orderedItems = OrderFactory.getOrderedItems();
             
             for (var i=0; i<orderedItems.length; i++) {
-                if (orderedItems[i].code === code) {
+                if (orderedItems[i].code === menuItem.code) {
                     orderedItems[i].quantity = orderedItems[i].quantity + 1;
                     itemFound = true;
                     break;
@@ -19,14 +19,9 @@
             };
             
             if (!itemFound) {
-                for (var i=0; i<this.getAllMenuItems().length; i++) {
-                    if(this.getAllMenuItems()[i].code === code) {
-                        orderedItems.push(this.getAllMenuItems()[i]);
+                        orderedItems.push(menuItem);
                         orderedItems[orderedItems.length-1].quantity = 1;
                         itemFound = false;
-                        break;
-                    }
-                };
             }
         };
     });
